aba453412c0530fcfd3f749a51f2ba9667fb4982
sunny <ethsok@protonmail.com>
2024-09-26 17:33:19 -0400

    Im done

--- slot.js
+++ slot.js
@@ -25,24 +25,9 @@ const deposit = () => {
     }
 };
 
-let depositAmount = deposit()
-
-
-// Step two number of lines to bed on.
-
-const numOfLines = () => {
-    console.log('1 = top row')
-    console.log('2 = middle row')
-    console.log('3 = bottom row')
-    const lineNum = prompt('What lines would you like to bet on?: [1,2,3] ')
-
-}
-
-const betLines = numOfLines()
-
-
 // Min and max bet 
 
+let wager
 const betAmount = () => {
     console.log('1 = max bet: 50')
     console.log('2 = min bet: 10')
@@ -50,77 +35,87 @@ const betAmount = () => {
     const minOrMax = prompt('Would you like to place a minimum bet or maximum bet?: [1 or 2] ')
     while(true){
         if (minOrMax == '1'){
-            depositAmount -= 50
-            console.log(`Current Balance: ${depositAmount}`)
+            wager = 50
             break
-
         }else if (minOrMax == '2'){
-            depositAmount -= 10
-            console.log(`Current Balance: ${depositAmount}`)
+            wager = 10
             break
         }else{
             console.log('Invalid Option')
             betAmount()
         }
     }
+    depositAmount -= wager
+        return wager
 }
 
-const minMax = betAmount()
-
-
 // display slot machine
-
-
 
 const reels = [
     ['🍒', '🍋', '🍊'],
     ['🔔', '💰', '🍒'],
     ['🍋', '🍊', '🔔'],
 ];
-
-// const slotDisplay = () => {
-    
-
-//     for(let o = 0; o < 3 ;o++){
-//         rowDisplay = ""
-//         for(let i = 0; i < 3; i++){
-//             rowDisplay += reels[o][i] + " "
-//         }
-//         console.log(rowDisplay)
-//     }
-// }
-
-// slotDisplay()
-
 // RNG generator for slotRandom 
 
 const RNG = () => {
     return Math.floor(Math.random() * (2 - 0) + 0)
+
  }
 
+ // Win conditions 
 
+const winConditions = (key) => {
+    if((key[0] == key[1]) && (key[0] == key[2])){
+        return true
+    } 
+}
+ 
 // Spin machine / randomize 
 
 const slotRandom = () => {
 
+    let win = false
    
     for(let o = 0; o < 3 ;o++){
         rowDisplay = ""
+        key = []
         for(let i = 0; i < 3; i++){
             const outer = RNG()
             const inner = RNG()
+
             rowDisplay += reels[outer][inner] + " "
+            key.push(reels[outer][inner])
+
         }
         console.log(rowDisplay)
+        win |= winConditions(key)
     }
-    console.log(`Current Balance: ${depositAmount}`)
+    if(win){
+        console.log('YOU WIN!!!')
+        depositAmount = wager * 2 + depositAmount
+        let winnings = wager * 2
+        console.log(depositAmount)
+        console.log(`+ ${winnings}`)
+
+    
+    }else{
+        console.log('YOU LOSE !')
+        console.log(depositAmount)
+        console.log(`- ${wager}`)
+    }
+
+   
+}
+const playAgain = () => {
+    const answer = prompt('Would you like to play again? Y OR N ')
+    return (answer.toUpperCase() == 'Y')
 }
 
+let depositAmount = deposit()
+do{
+const wager = betAmount()
 slotRandom()
+} while(playAgain())
 
-// Win conditions 
 
-const winConditions = () => {
-    if()
-}
- 
--- text.js
+++ text.js
@@ -6,43 +6,61 @@ const reels = [
     ['🍋', '🍊', '🔔'],
 ];
 
-const slotDisplay = () => {
+// const slotDisplay = () => {
     
 
-    for(let o = 0; o < 3 ;o++){
-        rowDisplay = ""
-        for(let i = 0; i < 3; i++){
-            rowDisplay += reels[o][i] + " "
-        }
-        console.log(rowDisplay)
-    }
-}
+//     for(let o = 0; o < 3 ;o++){
+//         rowDisplay = ""
+//         for(let i = 0; i < 3; i++){
+//             rowDisplay += reels[o][i] + " "
+//         }
+//         console.log(rowDisplay)
+//     }
+// }
 
-slotDisplay()
+// slotDisplay()
 
 // RNG generator for slotRandom 
 
 const RNG = () => {
     return Math.floor(Math.random() * (2 - 0) + 0)
+
  }
 
+ // Win conditions 
 
+const winConditions = (key) => {
+    if((key[0] == key[1]) && (key[0] == key[2])){
+        return true
+    } 
+}
+ 
 // Spin machine / randomize 
 
 const slotRandom = () => {
 
+    let win = false
    
     for(let o = 0; o < 3 ;o++){
         rowDisplay = ""
+        key = []
         for(let i = 0; i < 3; i++){
-
             const outer = RNG()
             const inner = RNG()
 
             rowDisplay += reels[outer][inner] + " "
+            key.push(reels[outer][inner])
+
         }
         console.log(rowDisplay)
+        win |= winConditions(key)
     }
+    if(win){
+        console.log('YOU WIN!!!')
+    }else{
+        console.log('YOU LOSE !')
+    }
+   
 }
 slotRandom()
 
